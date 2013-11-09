f53671d37f2a71520edc78315568ecd592e73521
majorarcana <nick.brownlow@gmail.com>
2013-11-09 16:13:29 +0000

    changes to sidebar to accomodate homepage portrait photo

--- sidebar.php
+++ sidebar.php
@@ -8,5 +8,12 @@
  */
 ?>
         <div id="col-b" class="column">
-
+            <aside>
+                <figure class="portrait">
+                    <img class="size-full wp-image-1330 " alt="Jon Snow, Channel 4 News Presenter and PRA Patron" src="http://www.prisonradioassociation.org/wp-content/uploads/2011/03/jon_snow.jpg" width="200" height="200" />
+                    <figcaption>Jon Snow, Channel 4 News Presenter &amp; PRA Patron</figcaption>
+                </figure>
+            <blockquote>
+             "I have chosen to lend my support to the PRA because I believe this is innovative broadcasting to a quite literally captive audience... the potential in all sorts of ways is enormous."
+             </blockquote>
         </div>
