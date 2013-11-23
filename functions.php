8f61dafc66807ced62f867dac1e5b75b49e45fd3
majorarcana <nick.brownlow@gmail.com>
2013-11-23 17:14:11 +0000

    Adding documentation to truncate

--- functions.php
+++ functions.php
@@ -6,8 +6,13 @@
  * @package PRA
  * @since 1.0
  */
- 
  class truncate {
+     /**
+      * Used to include page content in sub-index pages
+      * @param string $text Page content to be truncated
+      * @param integer $limit Number of characters to truncate to
+      * @return string $text Truncated page content
+      */
      public static function page_content($text, $limit){
          if(strlen($text) <= $limit) return $text;
          if(false !== ($break = strpos($text, ".", $limit))) {
